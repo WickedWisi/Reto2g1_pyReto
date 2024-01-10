289eccdef61e5ac03fc342aca2df9827e3588821
EnekoVazquez <enekotartanga123@gmail.com>
2024-01-10 13:28:28 +0100

    Sede completa con sus relaciones y restricciones.

--- addons/LOL/model/user.py
+++ addons/LOL/model/user.py
@@ -6,3 +6,7 @@ class User (models.Model):
 
     isVoluntario = fields.Boolean(string = "Voluntario")
     isSocio = fields.Boolean(string = "Socio")
+
+    voluntario_id = fields.Many2many("grupog1.Sede", string="Voluntario Id")
+    socio_id = fields.Many2One("grupog1.Sede", string="Socio Id")
+    evento_id = fields.One2Many("grupo1.Sede", string="Evento Id")
